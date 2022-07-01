0a0dd02ea412bcbad1393bf38ff69d819d29cf43
minhyuk <mh0952@gmail.com>
2022-07-01 14:41:47 +0900

    여섯번째 (talend 이용해서 db값 get, post, patch, delete 추가) talend에서 오류 떠서 @NoArgsConstructor 추가

--- src/main/java/com/example/test01/dto/ArticleForm.java
+++ src/main/java/com/example/test01/dto/ArticleForm.java
@@ -2,10 +2,14 @@ package com.example.test01.dto;
 
 import com.example.test01.entity.Article;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
+@Setter
 public class ArticleForm {
 
     private Long id; // id 필드 추가
