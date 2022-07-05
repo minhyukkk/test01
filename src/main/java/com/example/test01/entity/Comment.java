2da9e6b9b5ae9d77af4ad48e64febd5b2a13e66f
minhyuk <mh0952@gmail.com>
2022-07-05 23:44:22 +0900

    열번째(restapi를 완성, 댓글의 crud를 만들어봄)

--- src/main/java/com/example/test01/entity/Comment.java
+++ src/main/java/com/example/test01/entity/Comment.java
@@ -1,5 +1,6 @@
 package com.example.test01.entity;
 
+import com.example.test01.dto.CommentDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -30,4 +31,32 @@ public class Comment {
     @Column
     private String body;
 
+    public static Comment createComment(CommentDto dto, Article article) {
+        // 예외 처리
+        if (dto.getId() != null)
+            throw new IllegalArgumentException("댓글 생성 실패 댓글의 id가 없어야합니다.");
+        if (dto.getArticleId() != dto.getId())
+            throw new IllegalArgumentException("댓글 생성 실패 댓글의 id가 없어야합니다.");
+
+        // 엔티티 생성 및 반환
+        return new Comment(
+                dto.getId(),
+                article,
+                dto.getNickname(),
+                dto.getBody()
+        );
+    }
+
+    public void patch(CommentDto dto) {
+        // 예외 발생
+        if (this.id != dto.getId())
+            throw new IllegalArgumentException("댓글 수정 실패. 잘못된 id 입니다.");
+
+        // 객체 갱신
+        if (dto.getNickname() != null)
+            this.Nickname = dto.getNickname();
+
+        if (dto.getBody() != null)
+            this.body = dto.getBody();
+    }
 }
