25badad56252fad0958c8226ba32fa9df0a8c587
minhyuk <mh0952@gmail.com>
2022-07-08 14:12:31 +0900

    열 여섯번째(Ioc와 DI 연습)

---
+++ src/main/java/com/example/test01/ioc/Beef.java
@@ -0,0 +1,8 @@
+package com.example.test01.ioc;
+
+public class Beef extends Ingredient{
+
+    public Beef(String name) {
+        super(name);
+    }
+}
---
+++ src/main/java/com/example/test01/ioc/Pork.java
@@ -0,0 +1,10 @@
+package com.example.test01.ioc;
+
+
+public class Pork extends  Ingredient{
+
+    public Pork(String name) {
+        super(name);
+    }
+
+}
